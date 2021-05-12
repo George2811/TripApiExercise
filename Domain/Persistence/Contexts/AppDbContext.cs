0b6002c7df8ef532a03c5e41793c62ea67efbbf4
George2811 <sanchez28112001@gmail.com>
2021-05-12 16:01:26 -0500

    updated api

--- Domain/Persistence/Contexts/AppDbContext.cs
+++ Domain/Persistence/Contexts/AppDbContext.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Trips_Api.Domain.Models;
+using Trips_Api.Extensions;
 
 namespace Trips_Api.Domain.Persistence.Contexts
 {
@@ -28,6 +29,7 @@ namespace Trips_Api.Domain.Persistence.Contexts
             builder.Entity<Bookmark>().Property(b => b.Latitude).IsRequired();
             builder.Entity<Bookmark>().Property(b => b.Longitude).IsRequired();
 
+            builder.ApplySnakeCaseNamingConvention();
         }
     }
 }
